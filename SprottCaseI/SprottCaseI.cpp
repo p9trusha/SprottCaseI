8c4278d6aca0452c788b0336e30b4782bd416b94
p9trusha <petrnevolin2005@gmail.com>
2026-09-09 23:45:56 +0300

    Добавьте файлы проекта.

---
+++ SprottCaseI/SprottCaseI.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+double dx(double y)
+{
+    return -0.2 * y;
+}
+
+double dy(double x, double z)
+{
+    return x + z;
+}
+
+double dz(double x, double y, double z)
+{
+    return x + y * y - z;
+}
+
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
